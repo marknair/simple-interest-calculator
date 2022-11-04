5868018986649264c3920ac8d47df0cb0e1eba24
Mark Nair <marknair@me.com>
2022-11-04 13:39:30 +0000

    Formatting changes

--- script.js
+++ script.js
@@ -14,15 +14,12 @@ function compute() {
  var dateNow = new Date()
 	var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years)
 
-   var resultDisplay = document.getElementById("result");
-	resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>"  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+   var resultDisplay = document.getElementById("result")
+	resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>"  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>"
 }
 		
-function updateRate() 
-{
-	var rateval = document.getElementById("rate");
-	var output = document.getElementById("rate_val");
-	output.innerHTML = rateval.value + "%";
-	
-
+function updateRate() {
+	var rateval = document.getElementById("rate")
+	var output = document.getElementById("rate_val")
+	output.innerHTML = rateval.value + "%"
 }
